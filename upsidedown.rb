b48b8245207b0b3a77adabce39c8262538adda12
Nikolas Davis <ndavis@newrelic.com>
2015-01-16 11:21:56 -0800

    Upside down text

---
+++ upsidedown.rb
@@ -0,0 +1,25 @@
+class String
+  def upside_down
+    alphaUpDown = "ɐqɔpǝɟƃɥıɾʞlɯuodbɹsʇnʌʍxʎzɐqɔpǝɟƃɥıɾʞlɯuodbɹsʇnʌʍxʎz˙'¿¡⅋,„><][)(}{"
+    alpha = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ.,?!&'\"<>[](){}"
+    s = ""
+    self.each_char do |x|
+      if alpha.index(x) != nil
+        s += alphaUpDown[alpha.index(x)]
+      else
+        s += x
+      end
+    end
+    s.reverse
+  end
+end
+
+
+if ARGV.length == 0
+  abort "  Usage: You need to pass a string to me."
+end
+
+
+x = ARGV.join " "
+
+puts x.upside_down
